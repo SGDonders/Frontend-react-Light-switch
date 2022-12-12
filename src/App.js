f4d24f962fa710d1182882a14e512ca8f4d64219
S.G. Donders <you@example.com>
2022-12-12 15:03:00 +0100

    Added exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [ light, toggleLight] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className={light === true ? 'on' : 'off'}>
+        <section>
+          <div className="dot"></div>
+          <button type="button"
+                  onClick={() => toggleLight(!light)}>
+              {light
+                  ? <p>Turn off</p>
+                  : <p>Turn on</p>}
+              </button>
+        </section>
+      </main>
   );
 }
 
